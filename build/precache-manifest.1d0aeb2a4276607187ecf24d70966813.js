740ba3306bc5d8172b14e95603b73672737c0868
Evgenii Mishalov <forwow495800@gmail.com>
2019-06-19 14:47:27 +0500

    Dockerized build

---
+++ build/precache-manifest.1d0aeb2a4276607187ecf24d70966813.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a64d992ee0c00791466cdba896f57aa",
+    "url": "/static/media/background.3a64d992.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0699608a574105de6a37",
+    "url": "/static/js/main.cc1cf4c7.chunk.js"
+  },
+  {
+    "revision": "6104e905ccbd60514b4a",
+    "url": "/static/js/2.171951e9.chunk.js"
+  },
+  {
+    "revision": "0699608a574105de6a37",
+    "url": "/static/css/main.4ccb3f29.chunk.css"
+  },
+  {
+    "revision": "6104e905ccbd60514b4a",
+    "url": "/static/css/2.9f64965b.chunk.css"
+  },
+  {
+    "revision": "461d2516d1b7e9d4947751f7fadb3b52",
+    "url": "/index.html"
+  }
+];
